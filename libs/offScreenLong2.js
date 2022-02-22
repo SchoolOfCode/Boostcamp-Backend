96b379ff5ad463dd54aa771f1e6661f154a2ac85
atemp678 <ajt2158@columbia.edu>
2022-02-22 16:09:14 +0000

    added offscreenlong2

---
+++ libs/offScreenLong2.js
@@ -0,0 +1,10 @@
+const offScreenLong2 = [
+    {
+      taskInstructions:
+        'Go have dinner with a friend and write a poem!',
+      taskBenefits: 'Relationships are important for vitality!',
+    },
+  ];
+  
+  export default offScreenLong2;
+  
